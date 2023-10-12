a067dd31c9c6f3f7b068ba9728bcda9cff839849
İlker Civil <ilkercivil@gmail.com>
2023-10-12 10:29:21 +0300

    pen up ,pen down, function adede

--- main.py
+++ main.py
@@ -24,10 +24,17 @@ def clear_screen():
 def turtle_return_home():
     turtle_instance.home()
 
+def turtle_pen_up():
+    turtle_instance.penup()
+
+def turtle_pen_down():
+    turtle_instance.pendown()
+
 drawing_board.onkey(fun=turtle_forward,key="space")
 drawing_board.onkey(fun=rotate_angle_right,key="Down")
 drawing_board.onkey(fun=rotate_angel_left,key="Up")
 drawing_board.onkey(fun=clear_screen,key="c")
 drawing_board.onkey(fun=turtle_return_home,key="h")
-
+drawing_board.onkey(fun=turtle_pen_down,key="w")
+drawing_board.onkey(fun=turtle_pen_up,key="q")
 turtle.mainloop()
